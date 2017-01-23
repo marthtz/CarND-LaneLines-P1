484b3430995ff58d10f61799d0c0a346a049c34a
martin <mhintz@posteo.net>
2017-01-23 10:52:38 +1030

    Added code to detect basic lines. To be done now: add extrapolation, single lines, angle detection.

---
+++ P1_video.py
@@ -0,0 +1,189 @@
+#importing some useful packages
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import cv2
+import math
+import sys
+#%matplotlib inline
+
+# Some OpenCV functions (beyond those introduced in the lesson) that might be useful for this project are:
+
+# cv2.inRange() for color selection
+# cv2.fillPoly() for regions selection
+# cv2.line() to draw lines on an image given endpoints
+# cv2.addWeighted() to coadd / overlay two images cv2.cvtColor() to grayscale or change color cv2.imwrite() to output images to file
+# cv2.bitwise_and() to apply a mask to an image
+
+
+def grayscale(img):
+    """Applies the Grayscale transform
+    This will return an image with only one color channel
+    but NOTE: to see the returned image as grayscale
+    (assuming your grayscaled image is called 'gray')
+    you should call plt.imshow(gray, cmap='gray')"""
+    return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # Or use BGR2GRAY if you read an image with cv2.imread()
+    # return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+def canny(img, low_threshold, high_threshold):
+    """Applies the Canny transform"""
+    return cv2.Canny(img, low_threshold, high_threshold)
+
+def gaussian_blur(img, kernel_size):
+    """Applies a Gaussian Noise kernel"""
+    return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
+
+def region_of_interest(img, vertices):
+    """
+    Applies an image mask.
+    
+    Only keeps the region of the image defined by the polygon
+    formed from `vertices`. The rest of the image is set to black.
+    """
+    #defining a blank mask to start with
+    mask = np.zeros_like(img)   
+    
+    #defining a 3 channel or 1 channel color to fill the mask with depending on the input image
+    if len(img.shape) > 2:
+        channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
+        ignore_mask_color = (255,) * channel_count
+    else:
+        ignore_mask_color = 255
+        
+    #filling pixels inside the polygon defined by "vertices" with the fill color    
+    cv2.fillPoly(mask, vertices, ignore_mask_color)
+    
+    #returning the image only where mask pixels are nonzero
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
+
+
+def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
+    """
+    NOTE: this is the function you might want to use as a starting point once you want to 
+    average/extrapolate the line segments you detect to map out the full
+    extent of the lane (going from the result shown in raw-lines-example.mp4
+    to that shown in P1_example.mp4).  
+    
+    Think about things like separating line segments by their 
+    slope ((y2-y1)/(x2-x1)) to decide which segments are part of the left
+    line vs. the right line.  Then, you can average the position of each of 
+    the lines and extrapolate to the top and bottom of the lane.
+    
+    This function draws `lines` with `color` and `thickness`.    
+    Lines are drawn on the image inplace (mutates the image).
+    If you want to make the lines semi-transparent, think about combining
+    this function with the weighted_img() function below
+    """
+    for line in lines:
+        for x1,y1,x2,y2 in line:
+            cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+
+def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
+    """
+    `img` should be the output of a Canny transform.
+        
+    Returns an image with hough lines drawn.
+    """
+    lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
+    line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    draw_lines(line_img, lines)
+    return line_img
+
+# Python 3 has support for cool math symbols.
+
+def weighted_img(img, initial_img, α=0.8, β=1., λ=0.):
+    """
+    `img` is the output of the hough_lines(), An image with lines drawn on it.
+    Should be a blank image (all black) with lines drawn on it.
+    
+    `initial_img` should be the image before any processing.
+    
+    The result image is computed as follows:
+    
+    initial_img * α + img * β + λ
+    NOTE: initial_img and img must be the same shape!
+    """
+    return cv2.addWeighted(initial_img, α, img, β, λ)
+
+
+def process_image(image):
+    # 1. grayscale
+    gray = grayscale(image)
+
+    # 2. Blur
+    # Define a kernel size and apply Gaussian smoothing
+    kernel_size = 5
+    blur_gray = gaussian_blur(gray, kernel_size)
+
+    # 3. Canny edge
+    # Define our parameters for Canny and apply
+    low_threshold = 50
+    high_threshold = 150
+    edges = canny(blur_gray, low_threshold, high_threshold)
+
+
+    # 4. Region of interest (4-sided polygon)
+    # This time we are defining a four sided polygon to mask
+    imshape = image.shape
+    xPct = 0.05
+    yPct = 0.60
+
+    xbl = imshape[1] * xPct
+    xbr = imshape[1] * (1 - xPct)
+    xtl = imshape[1] * (0.5 - xPct)
+    xtr = imshape[1] * (0.5 + xPct)
+
+    yb = imshape[0]
+    yt = imshape[0] * yPct
+
+    vertices = np.array([[(xbl,yb),(xtl, yt), (xtr, yt), (xbr, yb)]],
+                        dtype=np.int32)
+
+    masked_image = region_of_interest(edges, vertices)
+
+
+    # 5. Hough lines
+    # Define the Hough transform parameters
+    # Make a blank the same size as our image to draw on
+    rho = 2 # distance resolution in pixels of the Hough grid
+    theta = np.pi/180 # angular resolution in radians of the Hough grid
+    threshold = 15     # minimum number of votes (intersections in Hough grid cell)
+    min_line_len = 20 #minimum number of pixels making up a line
+    max_line_gap = 30     # maximum gap in pixels between connectable line segments
+    
+    # Run Hough on edge detected image
+    # Output "lines" is an array containing endpoints of detected line segments
+    line_img = hough_lines(masked_image, rho, theta, threshold, min_line_len, max_line_gap)
+
+
+    # 6. Overlay Hough lines with original image
+    # Create a "color" binary image to combine with line image
+    overlayedImg = weighted_img(line_img, image, 0.8, 1, 0)
+
+    return overlayedImg
+
+
+
+################################################################################
+#
+#       Main
+#
+################################################################################
+
+# Import everything needed to edit/save/watch video clips
+from moviepy.editor import VideoFileClip
+from IPython.display import HTML
+
+video_output = 'white.mp4'
+clip1 = VideoFileClip("solidWhiteRight.mp4")
+
+video_output = 'yellow.mp4'
+clip1 = VideoFileClip('solidYellowLeft.mp4')
+
+video_output = 'challenge_out.mp4'
+clip1 = VideoFileClip('challenge.mp4')
+
+white_clip = clip1.fl_image(process_image) #NOTE: this function expects color images!!
+white_clip.write_videofile(video_output, audio=False)
